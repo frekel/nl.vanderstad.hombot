afdef58a8043577a826d4769648b5d40274c1107
Frank van der Stad <frank.vanderstad@flinders.nl>
2022-10-16 13:36:00 +0200

    Add flows: Fix the commands and the mode dropdown values

--- app.js
+++ app.js
@@ -13,11 +13,17 @@ class Hombot extends Homey.App {
     const modeActionCard = this.homey.flow.getActionCard('mode');
     modeActionCard.registerRunListener(async (args, state) => {
       this.log('Running the card: mode with args=%s and state=%s', args, state);
+      if ('mode' in args) {
+        this.log(`Execute the command: %7b%22COMMAND%22:%7b%22CLEAN_MODE%22:%22${args.mode}'%22%7d%7d'`);
+      }
     });
 
     const turboActionCard = this.homey.flow.getActionCard('turbo');
     turboActionCard.registerRunListener(async (args, state) => {
       this.log('Running the card: turbo with args=%s and state=%s', args, state);
+      if ('turbo' in args) {
+        this.log(`Execute the command: %7b%22COMMAND%22:%7b%22TURBO%22:%22${args.turbo}'%22%7d%7d'`);
+      }
     });
   }
 
