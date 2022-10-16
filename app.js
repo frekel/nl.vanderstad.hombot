b17860c5993bbcd9be9c1bcb7769a55bfb4a6447
Frank van der Stad <frank.vanderstad@flinders.nl>
2022-10-16 12:32:56 +0200

    Add flows: Fix listeners

--- app.js
+++ app.js
@@ -10,21 +10,6 @@ class Hombot extends Homey.App {
   async onInit() {
     this.log('Hombot App has been initialized');
 
-    const startActionCard = this.homey.flow.getActionCard('start');
-    startActionCard.registerRunListener(async (args, state) => {
-      this.log('Running the card: start with args=%s and state=%s', args, state);
-    });
-
-    const stopActionCard = this.homey.flow.getActionCard('stop');
-    stopActionCard.registerRunListener(async (args, state) => {
-      this.log('Running the card: stop with args=%s and state=%s', args, state);
-    });
-
-    const homeActionCard = this.homey.flow.getActionCard('home');
-    homeActionCard.registerRunListener(async (args, state) => {
-      this.log('Running the card: home with args=%s and state=%s', args, state);
-    });
-
     const modeActionCard = this.homey.flow.getActionCard('mode');
     modeActionCard.registerRunListener(async (args, state) => {
       this.log('Running the card: mode with args=%s and state=%s', args, state);
