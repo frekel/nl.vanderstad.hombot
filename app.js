05d0b6b9b4fbc4b66c4db8be6d713e7d3b39ad38
Frank van der Stad <frank.vanderstad@flinders.nl>
2022-10-16 11:45:49 +0200

    Add flows

--- app.js
+++ app.js
@@ -7,8 +7,33 @@ class Hombot extends Homey.App {
   /**
    * onInit is called when the app is initialized.
    */
-  async onInit() {
-    this.log('MyApp has been initialized');
+  async onInit(eventName, listener) {
+    this.log('Hombot App has been initialized');
+
+    const startActionCard = this.homey.flow.getActionCard('start');
+    startActionCard.addListener(async (args, state) => {
+      this.log('Running the card: start with args=%s and state=%s', args, state);
+    }, listener);
+
+    const stopActionCard = this.homey.flow.getActionCard('stop');
+    stopActionCard.addListener(async (args, state) => {
+      this.log('Running the card: stop with args=%s and state=%s', args, state);
+    }, listener);
+
+    const homeActionCard = this.homey.flow.getActionCard('home');
+    homeActionCard.addListener(async (args, state) => {
+      this.log('Running the card: home with args=%s and state=%s', args, state);
+    }, listener);
+
+    const modeActionCard = this.homey.flow.getActionCard('mode');
+    modeActionCard.addListener(async (args, state) => {
+      this.log('Running the card: mode with args=%s and state=%s', args, state);
+    }, listener);
+
+    const turboActionCard = this.homey.flow.getActionCard('turbo');
+    turboActionCard.addListener(async (args, state) => {
+      this.log('Running the card: turbo with args=%s and state=%s', args, state);
+    }, listener);
   }
 
 }
